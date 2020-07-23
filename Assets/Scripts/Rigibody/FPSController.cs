82e3cfb90c8a01778567e12f0137623a90435208
Ernesto <ernestoda94@gmail.com>
2020-07-23 11:29:21 +0200

    Change Gravity mechanic V2 and pick up objects.

--- Assets/Scripts/Rigibody/FPSController.cs
+++ Assets/Scripts/Rigibody/FPSController.cs
@@ -1,116 +1,225 @@
-﻿
-using System.Collections;
-
+﻿using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
-
-
 public class FPSController : MonoBehaviour
-
 {
+    private Rigidbody rigidbody;
 
-    CharacterController characterController;
+    [Header("Movement")]
+    public float speed = 5;
+    public float jumpForce = 15;
+    private float xAxis, zAxis;
 
-    [Header("Opciones de personaje")]
-    public float walkSpeed = 6.0f;
-    public float runSpeed = 10.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public Vector3 gravityV3 = new Vector3(0, 20f, 0);
-    public Vector3 gravityV3Inverted = new Vector3(0, -20f, 0);
+    [Header("Ground things")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask scenarioMask;
+    bool isGrounded;
 
-    [Header("Opciones de camara")]
-    public Camera cam;
-    public float mouseHorizontal = 3.0f;
-    public float mouseVertical = 2.0f;
-    public float minRotation = -90.0f;
-    public float maxRotation = 90.0f;
-    float inputHorizontalMouse, inputVerticalMouse;
+    [Header("Gravity things")]
+    public float gravity = -9.81f;
+    [Tooltip("Time the player is rotating")]
+    public float rotationTime = 1f;
+    private int gravityID = 2; //controles what "gravity position" player is(1-up, 2d-own, 3-left o 4-right) Used to avoid change to the smae gravity twice
+    private bool changeGravityButtonDown = false;
 
-
-    private Vector3 move = Vector3.zero;
-
+    // Start is called before the first frame update
     void Start()
-
     {
-        characterController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-
-
+    // Update is called once per frame
     void Update()
-
     {
-        //cambio de gravedad
-        /*if (Input.GetKeyDown(KeyCode.G))//cambiar el key por button
-        {
-            Debug.Log("Pulsado G");
-            //gravity = -gravity;
-            gravity = Mathf.Lerp(gravity, -gravity, 2000f );
-            //gravityV3 = Vector3.Lerp(gravityV3, -gravityV3, 2000);
-        }*/
 
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            StartCoroutine(ChangeGravity(gravity, -gravity, 5f));
-        }
-       
+        canChangeGravity();
 
-        //Movimiento de camara
-        inputHorizontalMouse = mouseHorizontal * Input.GetAxis("Mouse X");
-        inputVerticalMouse += mouseVertical * Input.GetAxis("Mouse Y");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, scenarioMask);
 
-        inputVerticalMouse = Mathf.Clamp(inputVerticalMouse, minRotation, maxRotation);
-        cam.transform.localEulerAngles = new Vector3(-inputVerticalMouse, 0, 0);
-        transform.Rotate(0, inputHorizontalMouse, 0);
+        movement();
 
-        //Movimiento del personaje
-        if (characterController.isGrounded)
+        jump();
 
-        {
-            move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-
-            if (Input.GetKey(KeyCode.LeftShift))//cambiar el key por button
-            {
-                move = transform.TransformDirection(move) * runSpeed;
-            }
-            else
-            {
-                move = transform.TransformDirection(move) * walkSpeed;
-            }
-
-
-            if (Input.GetKey(KeyCode.Space))//cambiar el key por button
-            {
-                move.y = jumpSpeed;
-            }
-        }
-
-        move.y -= gravity * Time.deltaTime;
-        //move.y -= gravityV3.y * Time.deltaTime;
-
-        characterController.Move(move * Time.deltaTime);
-
+        //changeGravity();
+        changeGravityV2();
     }
 
-    //Corrutina
-    IEnumerator ChangeGravity(float gravityStart, float gravityEnd, float duration)
+    void movement()
     {
-        float elapsed = 0.0f;
-        while (elapsed < duration)
+        xAxis = Input.GetAxis("Horizontal");
+        zAxis = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(xAxis, 0, zAxis);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+
+    void jump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
         {
-            gravity = Mathf.Lerp(gravityStart, gravityEnd, elapsed / duration);
-            //transform.Rotate(0,0, Mathf.Lerp(0, -90, duration));
-            elapsed += Time.deltaTime;
+            rigidbody.AddRelativeForce(0, jumpForce, 0);
+        }
+    }
+
+    void changeGravity()
+    {
+        if (canChangeGravity())
+        {
+            //if (Input.GetKeyDown(KeyCode.Alpha1) /*&& isGrounded*/ )//cambiar el key por button
+            if (Input.GetButtonDown("GravityUp") && gravityID != 1)
+            {
+                Physics.gravity = new Vector3(0, gravity, 0);//gravity goes up
+                StartCoroutine(RotateFixedDegrees(Vector3.back * 180, rotationTime));
+                StartCoroutine(RepositionZAngle(180, rotationTime + 0.01f));
+                gravityID = 1;//"flag"
+            }
+            else if (Input.GetButtonDown("GravityDown") && gravityID != 2)
+            {
+                Physics.gravity = new Vector3(0, -gravity, 0);//gravity goes down
+                StartCoroutine(RotateFixedDegrees(Vector3.back * 0, rotationTime));
+                StartCoroutine(RepositionZAngle(0, rotationTime + 0.01f));
+                gravityID = 2;//"flag"
+            }
+            else if (Input.GetButtonDown("GravityLeft") && gravityID != 3)
+            {
+                Physics.gravity = new Vector3(-gravity, 0, 0);//gravity goes left
+                StartCoroutine(RotateFixedDegrees(Vector3.back * 90, rotationTime));
+                StartCoroutine(RepositionZAngle(-90, rotationTime + 0.01f));
+                gravityID = 3;//"flag"
+            }
+            else if (Input.GetButtonDown("GravityRight") && gravityID != 4)
+            {
+                Physics.gravity = new Vector3(gravity, -0, 0);//gravity goes right
+                StartCoroutine(RotateFixedDegrees(Vector3.back * -90, rotationTime));
+                StartCoroutine(RepositionZAngle(90, rotationTime + 0.01f));
+                gravityID = 4;//"flag"
+            }
+        }
+    }
+
+    void changeGravityV2()
+    {
+        if (canChangeGravity())
+        {
+            /////
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                gravityID--;
+                changeGravityButtonDown = true;
+              
+            }else if(Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                gravityID += 2;
+                changeGravityButtonDown = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+                {
+                gravityID++;
+                changeGravityButtonDown = true;
+            }
+            ////
+            if (gravityID == 0)
+            {
+                gravityID = 4;
+            }else if (gravityID == 5)
+            {
+                gravityID = 1;
+            }
+            else if (gravityID == 6)
+            {
+                gravityID = 2;
+            }
+            //////
+            if (changeGravityButtonDown == true)//if press Q, 2 or E
+            {
+                if (gravityID == 4) /*&& isGrounded*/ //cambiar el key por button
+                                                      //if (Input.GetButtonDown("GravityUp") && gravityID != 1)
+                {
+                    Physics.gravity = new Vector3(0, gravity, 0);//gravity goes up
+                    StartCoroutine(RotateFixedDegrees(Vector3.back * 180, rotationTime));
+                    StartCoroutine(RepositionZAngle(180, rotationTime + 0.01f));
+                    //gravityID = 4;//"flag"
+                    changeGravityButtonDown = false;
+                }
+                else if (gravityID == 2) /*&& gravityID != 2*/
+                {
+                    Physics.gravity = new Vector3(0, -gravity, 0);//gravity goes down
+                    StartCoroutine(RotateFixedDegrees(Vector3.back * 0, rotationTime));
+                    StartCoroutine(RepositionZAngle(0, rotationTime + 0.01f));
+                    //gravityID = 2;//"flag"
+                    changeGravityButtonDown = false;
+                }
+                else if (gravityID == 1)
+                {
+                    Physics.gravity = new Vector3(-gravity, 0, 0);//gravity goes left
+                    StartCoroutine(RotateFixedDegrees(Vector3.back * 90, rotationTime));
+                    StartCoroutine(RepositionZAngle(-90, rotationTime + 0.01f));
+                    //gravityID = 1;//"flag"
+                    changeGravityButtonDown = false;
+                }
+                else if (gravityID == 3)
+                {
+                    Physics.gravity = new Vector3(gravity, -0, 0);//gravity goes right
+                    StartCoroutine(RotateFixedDegrees(Vector3.back * -90, rotationTime));
+                    StartCoroutine(RepositionZAngle(90, rotationTime + 0.01f));
+                    //gravityID = 3;//"flag"
+                    changeGravityButtonDown = false;
+                }
+            }
+
+        }
+    }
+    bool canChangeGravity()
+    {
+        //if (isGrounded && (transform.localEulerAngles.z == 90 || transform.localEulerAngles.z == -90 || transform.localEulerAngles.z == 180 || transform.localEulerAngles.z == 0))
+        if (isGrounded && (transform.localEulerAngles.z == 90 || transform.localEulerAngles.z == 270 || transform.localEulerAngles.z == 180 || transform.localEulerAngles.z == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void repositionZAngle()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 90);
+    }
+
+    #region COROUTINES
+    //COROUTINES
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)//rotate X degrees in X seconds. Does not used
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
             yield return null;
         }
-        gravity = gravityEnd;
     }
 
+    IEnumerator RotateFixedDegrees(Vector3 finalRotation, float inTime)//rotate to Xposition in X seconds. Used
+    {
+        
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(finalRotation);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+  
+    }
+
+    //change Z degrees rotation after X seconds
+    //Used to fixed the rotation after change gravity. Some how sometimes does not rotate 90º but 89.81º 
+    IEnumerator RepositionZAngle(int degrees, float delayTime)
+    {    
+        yield return new WaitForSeconds(delayTime);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, degrees);
+    }
+    #endregion
 }
-
-
-
---
+++ Assets/Scripts/Rigibody/PickObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickObject : MonoBehaviour
+{
+
+    public Transform pickObjectDestination;
+    public float pickDistance = 3f;
+
+
+    private void OnMouseDown()
+    {
+        if(Vector3.Distance(transform.position, pickObjectDestination.root.position) < pickDistance)//YOu cannot pick up the object if you are too far away
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = pickObjectDestination.position;
+            this.transform.parent = GameObject.Find("PickObject").transform;
+        }     
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+    }
+}
+
